fe3a6f845423a4367bf6442a81256c54cac1408d
kandavel-umapathy <kandaswamy197svg@gmail.com>
2023-05-16 23:48:37 +0530

    Intial Commit

---
+++ Upstox/Upstox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Upstox
+//
+//  Created by kandavel on 16/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
